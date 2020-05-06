03f26d71d7de29520b65e84aafb496bfe5c31214
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-06 10:58:36 -0600

    added readme file

--- my_file.py
+++ my_file.py
@@ -2,6 +2,6 @@ print('Hi Git')
 
 # Use git init at the root of the project.
 # Git file will have a "." in front of it, to see it, turn on the hidden files.
-# Use git status to see untracked files.
+# Use git status to see untracked files. red are untracked, green are tracked but not commited.
 # Use git add . to add files to the repository.
-# 
+# Use git commit -m "enter description here" to commit the files and folders to Git.
