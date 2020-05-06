37de368d3c485807ba41cf4ce64acb60e8a99fd5
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-06 10:08:06 -0600

    initial commit

---
+++ my_file.py
@@ -0,0 +1,7 @@
+print('Hi Git')
+
+# Use git init at the root of the project.
+# Git file will have a "." in front of it, to see it, turn on the hidden files.
+# Use git status to see untracked files.
+# Use git add . to add files to the repository.
+# 
