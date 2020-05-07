5fc84c03449c307abf16bc80a6bab9eb938dd7e3
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-06 22:34:40 -0600

    updated readme

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 > My code for the git project
 >Now I've changed a few things.
+
+
+>I'm in the feature branch.
--- my_file.py
+++ my_file.py
@@ -5,4 +5,11 @@ print('Hi Git')
 # Use git status to see untracked files. red are untracked, green are tracked but not commited.
 # Use git add . to add files to the repository.
 # Use git commit -m "enter description here" to commit the files and folders to Git.
+# Use git push to advance changes up to gitHub for version control.
+# Use .gitignore file to list the names of files and folders to ignore during the push. 
+# Add a / after folder names to include all files within the folder for ignore.
+# Use git pull to bring changes made in gitHub down to your local machine.
+
+
+
 print('Hi from another dev')
